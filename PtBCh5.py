8cb91b27ea87850cbe37681702ef153e9b8c9c37
gsynetos-cjob <g.synetos@c-job.eu>
2017-02-24 10:40:45 +0100

    Add files via upload

---
+++ PtBCh5.py
@@ -0,0 +1,250 @@
+from math import sqrt, exp, pi
+
+# ------------------------
+# CHAPTER 5
+# ------------------------
+
+# ------------------------
+# Section 1
+# ------------------------
+
+
+def navigationcoefficients (coefficient, navigationnotation):
+    """Calculates the navigation coefficients n and n1 as per NR467 BV,PtB,Ch5,Sec1
+
+       Input:
+       type: Navigation Coefficient (n or n1)
+       navigation notation: 'unrestricted navigation'
+                            'summer zone'
+                            'tropical zone'
+                            'coastal zone'
+                            'sheltered area'
+    """
+
+    if coefficient == 'n':
+        dict = {'unrestricted navigation': 1, 'summer zone': 0.9, 'tropical zone': 0.8, 'coastal zone': 0.8, 'sheltered area': 0.65}
+        return dict[navigationnotation]
+    if coefficient == 'n1':
+        dict = {'unrestricted navigation': 1, 'summer zone': 0.95, 'tropical zone': 0.9, 'coastal zone': 0.9, 'sheltered area': 0.8}
+        return dict[navigationnotation]
+
+# ------------------------
+# Section 2
+# ------------------------
+
+
+def waveparameterc(L):
+    """Calculates the wave parameter C as per NR467 BV,PtB,Ch5,Sec2
+
+       Input:
+       L: Rule Lenght [m] """
+
+    if 65 <= L < 90:
+        return (118 - 0.36 * L) * (L / 1000)
+    if 90 <= L < 300:
+        return 10.75 - ((300-L) / 100) ** 1.5
+    if 300 <= L <= 350:
+        return 10.75
+    if L > 350:
+        return 10.75 - ((L-350) / 150) ** 1.5
+
+
+def waveparameterh(L):
+    """Calculates the wave parameter C as per NR467 BV,PtB,Ch5,Sec2
+
+       Input:
+       L: Rule Lenght [m] """
+    return 8.13 - ((250 - 0.7 * L) / 125) ** 3
+
+# ------------------------
+# Section 3
+# ------------------------
+
+
+def shiprelativemotionupright(x, cb, n, C, L, T, D, t1):
+
+    """Calculates the reference value h1
+    of the relative motion according to NR467 BV,PtB,Ch5,Sec3,[3.3]
+
+    srm_u "Ship Relative Motion in upright condition"
+    Input
+    ------
+    C_b : Block Coefficient
+    n: navigation coeffecient as per NR467 BV,PtB,Ch5,Sec1,[2.6]
+    C: Wave Parameter NR467 BV,PtB,Ch5,Sec2
+    L: Rule Lenght [m]
+    T: Design Draught [m]
+    D: Depth [m]
+    T1: Draught associated with each loading condition [m]
+
+    Output
+    -------
+    h1: ship relative motion reference value"""
+
+    h1_M = 0.42 * n * C * (cb + 0.7)
+    if x == 0:
+        if cb < 0.875:
+            return 0.7 *((4.35 / sqrt(cb)) - 3.25) * h1_M
+        return h1_M
+    if x < 0.3 * L:
+        h1_AE = 0.7 * ((4.35 / sqrt(cb)) - 3.25) * h1_M
+        return h1_AE - ((h1_AE - h1_M) / 0.3) * (x/L)
+    if x < 0.7 * L:
+        limit = min(t1, D - 0.9 * T)
+        return min(0.42 * n * C * (cb + 0.7), limit)
+    if x < L:
+        h1_FE = ((4.35 / sqrt(cb)) - 3.25) * h1_M
+        return h1_M + ((h1_FE - h1_M) / 0.3)*((x / L)-0.7)
+    return ((4.35 / sqrt(cb)) - 3.25) * h1_M
+
+
+
+# ------------------------
+# Section 5
+# ------------------------
+
+
+def seastillwaterpressure(z, t1, rho=1.025, g=9.81):
+
+    """Calculates the still water  sea pressure as per NR467 BV,PtB,Ch5,Sec5,[1]
+
+    Input
+    ------
+    z: considered location [m]
+    T1: Draught associated with each loading condition [m]
+    rho: sea water density []
+    g: gravity accelearation [m/s^2]
+
+    Output
+    -------
+    ps: still water pressure in [kN/m^2]"""
+
+    if z <= t1:
+        return rho * g * (t1 - z)
+    else:
+        return 0
+
+
+def seawavepressuresidesandbottom(z, t1, loadcase, h1, L, D, rho=1.025, g=9.81):
+
+    """ Calculates the wave sea pressure on bottom and sides according to NR467 BV,PtB,Ch5,Sec5,[2]
+
+    Input
+    ------
+    z: considered location [m]
+    T1: draught [m]
+    loadcase: Load Case ('a+' , 'a-' , 'b' , 'c', 'd')
+    L: Rule Length [m]
+    h1: Reference Values of the ship relative motions in the upright ship condition, defined in Ch5,Sec3,[3.3]
+    rho: sea water density [t/m^3]
+    g: gravity acceleration [m/s^2]
+
+    Output
+    -------
+    pw: sea wave pressure in [kN/m^2]
+    """
+
+    cf1 = cf1combinationfactor(loadcase)
+    phi1 = 1
+    phi2 = phi2coefficient(L)
+
+    if loadcase == 'a+':
+        if z <= t1:
+            return rho * g * cf1 * h1 * exp((-2 * pi * (t1 - z) / L))
+        if t1 < z <= D:
+            limit = 0.15 * phi1 * phi2 * L
+            return max(rho * g * (t1 + h1 - z), limit)
+
+    if loadcase == 'a-':
+        if z <= t1:
+            return max(-rho * g * h1 * exp((-2 * pi * (t1 - z) / L)), rho * g * (z - t1))
+        if t1 < z <= D:
+            return 0
+
+    if loadcase == 'b':
+        if z <= t1:
+            return rho * g * cf1 *h1 * exp((-2 * pi * (t1 - z) / L))
+        if t1 < z <= D:
+            limit = 0.15 * phi1 * phi2coefficient(L) * L
+            return max(rho * g * (t1 + h1 - z), limit)
+
+
+def seastillwaterpressureexposeddecks(L):
+    phi1 = 1
+    phi2 = phi2coefficient(L)
+    return 10 * phi1 * phi2
+
+def seawavepressureexposeddecks(x, z, t1, loadcase, L, n, cb, servicespeed = 13):
+
+    """ Calculates the wave sea pressure on exposed deck according to NR467 BV,PtB,Ch5,Sec5,[Tab4]
+
+    Input
+    ------
+    x: x-coordinate [m]
+    z: considered location [m]
+    t1: draught [m]
+    loadcase: Load Case ('a+' , 'a-' , 'b', 'c', 'd')
+    L: Rule Length [m]
+    servicespeed: maximum ahead service speed [kn] not less than 13
+    n: navigation coefficient
+    C_b: block coefficient
+    h1: Reference Values of the ship relative motions in the upright ship condition, defined in Ch5,Sec3,[3.3]
+
+    Output
+    -------
+    pw: sea wave pressure in exposed decks in [kN/m^2]
+    """
+
+    phi1 = 1
+    phi2 = phi2coefficient(L)
+    v = max(servicespeed, 13)
+
+    if loadcase == 'a+' or loadcase == 'b':
+        if 0 <= x <= 0.5 * L:
+            return 17.5 * n * phi1 * phi2
+        if 0.5 * L < x < 0.75 * L:
+            hf = hformula(0.75 * L, L, v, cb, z, t1, loadcase)
+            return (17.5 + ((19.6 * sqrt(hf) - 17.5) / 0.25) * ((x / L) - 0.5))* n * phi1 * phi2
+        if 0.75 * L <= x <= L:
+            h = hformula(x, L, v, cb, z, t1, loadcase)
+            return 19.6 * n * phi1 * phi2 * sqrt(h)
+
+    if loadcase == 'a-':
+        return 0
+
+
+def phi2coefficient(L):
+    """ Calculates the phi 2 coefficient, used for the calculation of the wave pressure NR467 BV,PtB,Ch5,Sec5,[Tab4]
+
+        Input
+        ------
+        L: Rule Length [m]
+
+        Output
+        -------
+        phi2
+    """
+
+    if 0 < L < 120:
+        return L / 120
+    if L >= 120:
+        return 1
+
+
+def cf1combinationfactor(loadcase):
+    if loadcase == 'a+' or loadcase == 'a-':
+        return 1
+    if loadcase == 'b':
+        return 0.5
+
+
+def hformula(x, L, v, cb, z, t1, loadcase):
+    return max (cf1combinationfactor(loadcase) *  ((2.66 * ((x / L) - 0.7) ** 2 ) + 0.14) * sqrt((v * L ) / cb) - (z - t1), 0.8)
+
+
+
+
+
+
+
+
---
+++ PtDCh13.py
@@ -0,0 +1,86 @@
+from math import sin
+
+# ------------------------
+# CHAPTER 13
+# ------------------------
+
+def delta(pd, vd):
+    """Calculates the specific gravity of the mixture of sea water and spoil
+
+    Input
+    -----
+    pd: Maximum mass, of the spoil contained in the hopper space [t]
+    vd: Volume of the hopper space limited to the highest weir level [m^3]
+
+    Output
+    ------
+    delta
+    """
+    return pd / vd
+
+# ------------------------
+# Section 2
+# ------------------------
+
+
+def delta1(delta, alpha=0):
+
+    if delta < 1.4:
+        return delta
+    if delta >= 1.4:
+        return delta + (1.4 - delta) * sin(alpha) ** 2
+
+
+def still_water_pressure_hopper_well(dd, delta1, g=9.81):
+
+    """ Calculates the still water pressure as per NR467 BV,PtD,Ch13,Sec2,[3.5.1]
+
+    The still water pressure to be used in connection with inertial pressure in NR467 BV,PtD,Ch13,Sec2,[3.5.2]
+
+    Input
+    -----
+    delta1:
+    g:  gravity acceleration [m/s^2]
+    dd: vertical distance from the calculation point to the highest weir level with the corresponding specific gravity
+        of the mixture of sea water and spoil [m]
+    alpha: angle between the horizontal plane and the surface of the hull structure to which the calculation point
+        belongs [deg]
+
+    Output
+    ------
+    ps: still water pressure [kN/m^2]
+    """
+
+    return max(g * delta1 * dd, 11)
+
+
+def inertia_pressure_hopper_well():
+    """ Calculates the inertial pressure as per NR467 BV,PtD,Ch13,Sec2,[3.5.2],Tab6
+
+    Input
+    -----
+    loadcase: a+, a-, b, c, d
+    delta1: coefficient related to delta and angle alhpa
+    dd: vertical distance from the calculation point to the highest weir level with the corresponding specific gravity
+        of the mixture of sea water and spoil [m]
+
+    Output
+    ------
+    ps: still water pressure [kN/m^2]
+
+    """
+
+    if loadcase == 'a+' or 'a-':
+        return 0
+    if loadcase == 'b':
+        #to be calculated
+        pass
+    if loadcase == 'c':
+        #to be calculated
+        pass
+    if loadcase == 'd':
+        #to be calulated
+        pass
+
+
+
